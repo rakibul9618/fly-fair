86b83d68c84dd6ea7f1ef101daf40334cf2c2422
rakibul1896 <rhkhan9618@gmail.com>
2022-05-20 21:35:20 +0600

    some changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -120,7 +120,6 @@ const ResponsiveAppBar = () => {
                 mr: 2,
                 display: { xs: 'flex', md: 'none' },
                 flexGrow: 1,
-                fontFamily: 'monospace',
                 fontWeight: 700,
                 letterSpacing: '.3rem',
                 color: 'inherit',
