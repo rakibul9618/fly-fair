977e40930ac2bdc85bcbe200568940b5c9ec8bd4
rakibul9618 <rhkhan9618@gmail.com>
2022-05-21 18:28:37 +0600

    completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -61,191 +61,202 @@ const Footer = () => {
     >
       <Box
         component="div"
-        sx={{
-          display: "flex",
-          justifyContent: { xs: "space-around", xl: "space-between" },
-          flexWrap: "wrap",
-          px: { xs: "20px", sm: "30px", md: "40px", lg: "60px", xl: "100px" },
-        }}
+        sx={{ display: "flex", justifyContent: "space-around" }}
       >
         <Box
           component="div"
-          sx={{ padding: { xs: 2, lg: "30px 20px", xl: "16px 10px" } }}
+          sx={{
+            display: "flex",
+            justifyContent: { xs: "space-around", xl: "space-between" },
+            flexWrap: "wrap",
+            px: { xs: "20px", sm: "30px", md: "40px", lg: "60px", xl: "100px" },
+            maxWidth: "1336px",
+          }}
         >
-          <Box component="img" src={logo} alt="logo" />
-          <Box component="div" sx={{ py: 1 }}>
-            {socialIcon.map((value, index) => {
+          <Box
+            component="div"
+            sx={{ padding: { xs: 2, lg: "30px 20px", xl: "16px 10px" } }}
+          >
+            <Box component="img" src={logo} alt="logo" />
+            <Box component="div" sx={{ py: 1 }}>
+              {socialIcon.map((value, index) => {
+                return (
+                  <Box
+                    component="img"
+                    key={index}
+                    src={value.img}
+                    alt={value.alt}
+                    sx={{ paddingRight: "5px" }}
+                  />
+                );
+              })}
+            </Box>
+          </Box>
+          <Box
+            component="div"
+            sx={{ padding: { xs: 2, lg: "30px 20px", xl: "16px 10px" } }}
+          >
+            <Typography
+              component="h1"
+              sx={{
+                fontSize: "18px",
+                fontWeight: "bold",
+                paddingBottom: "10px",
+                color: "#202020",
+              }}
+            >
+              Company
+            </Typography>
+            {para.map((data, index) => {
               return (
-                <Box
-                  component="img"
+                <Typography
                   key={index}
-                  src={value.img}
-                  alt={value.alt}
-                  sx={{ paddingRight: "5px" }}
-                />
+                  component="h1"
+                  sx={{ fontSize: "16px", color: "#636363" }}
+                >
+                  {data}
+                </Typography>
               );
             })}
           </Box>
-        </Box>
-        <Box
-          component="div"
-          sx={{ padding: { xs: 2, lg: "30px 20px", xl: "16px 10px" } }}
-        >
-          <Typography
-            component="h1"
-            sx={{
-              fontSize: "18px",
-              fontWeight: "bold",
-              paddingBottom: "10px",
-              color: "#202020",
-            }}
-          >
-            Company
-          </Typography>
-          {para.map((data, index) => {
-            return (
-              <Typography
-                key={index}
-                component="h1"
-                sx={{ fontSize: "16px", color: "#636363" }}
-              >
-                {data}
-              </Typography>
-            );
-          })}
-        </Box>
-        <Box
-          component="div"
-          sx={{
-            padding: { xs: 2, lg: "30px 20px", xl: "16px 10px" },
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Typography
-            component="h1"
-            sx={{
-              fontSize: "18px",
-              fontWeight: "bold",
-              paddingBottom: "10px",
-              color: "#202020",
-            }}
-          >
-            Authorised by
-          </Typography>
-          <img src={iata} alt="IATA company logo" />
-        </Box>
-        <Box
-          component="div"
-          sx={{
-            padding: { xs: 2, lg: "30px 20px", xl: "16px 10px" },
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Typography
-            component="h1"
-            sx={{
-              fontSize: "18px",
-              fontWeight: "bold",
-              paddingBottom: "10px",
-              color: "#202020",
-            }}
-          >
-            Members of
-          </Typography>
-          <img
-            src={toad}
-            alt="TOAB company logo"
-            style={{ padding: "10px 0" }}
-          />
-          <img src={atab} alt="ATAB company logo" />
-        </Box>
-        <Box
-          component="div"
-          sx={{
-            padding: { xs: 2, lg: "30px 20px", xl: "16px 10px" },
-            alignItems: "center",
-          }}
-        >
-          <Typography
-            component="h1"
-            sx={{
-              fontSize: "18px",
-              fontWeight: "bold",
-              paddingBottom: "10px",
-              color: "#202020",
-            }}
-          >
-            Approved Agent
-          </Typography>
-          <img src={biman} alt="Biman company logo" />
-        </Box>
-        <Box
-          component="div"
-          sx={{
-            padding: { xs: 2, lg: "30px 20px", xl: "16px 10px" },
-            display: "flex",
-            flexDirection: "column",
-            alignItems: { xs: "center", xl: "start" },
-            px: { lg: "117px", xl: "" },
-          }}
-        >
-          <Typography
-            component="h1"
-            sx={{
-              fontSize: "18px",
-              fontWeight: "bold",
-              paddingBottom: "10px",
-              color: "#202020",
-            }}
-          >
-            Send us massage via Email
-          </Typography>
-          <Typography
-            component="p"
-            sx={{
-              fontSize: "16px",
-              color: "#636363",
-              width: "283px",
-              textAlign: { xs: "center", xl: "start" },
-            }}
-          >
-            It is a long established fact that a reader will be by content.
-          </Typography>
           <Box
             component="div"
             sx={{
+              padding: { xs: 2, lg: "30px 20px", xl: "16px 10px" },
               display: "flex",
-              alignItems: { sm: "center" },
-              justifyContent: "space-between",
-              flexDirection: { xs: "column", sm: "row" },
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            <Box
+            <Typography
               component="h1"
               sx={{
-                border: "2px solid #003566",
-                borderRadius: "4px",
-                width: "auto",
-                marginRight: { sm: "15px" },
+                fontSize: "18px",
+                fontWeight: "bold",
+                paddingBottom: "10px",
+                color: "#202020",
               }}
             >
-              <Typography
-                component="p"
+              Authorised by
+            </Typography>
+            <img src={iata} alt="IATA company logo" />
+          </Box>
+          <Box
+            component="div"
+            sx={{
+              padding: { xs: 2, lg: "30px 20px", xl: "16px 10px" },
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              component="h1"
+              sx={{
+                fontSize: "18px",
+                fontWeight: "bold",
+                paddingBottom: "10px",
+                color: "#202020",
+              }}
+            >
+              Members of
+            </Typography>
+            <img
+              src={toad}
+              alt="TOAB company logo"
+              style={{ padding: "10px 0" }}
+            />
+            <img src={atab} alt="ATAB company logo" />
+          </Box>
+          <Box
+            component="div"
+            sx={{
+              padding: { xs: 2, lg: "30px 20px", xl: "16px 10px" },
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              component="h1"
+              sx={{
+                fontSize: "18px",
+                fontWeight: "bold",
+                paddingBottom: "10px",
+                color: "#202020",
+              }}
+            >
+              Approved Agent
+            </Typography>
+            <img src={biman} alt="Biman company logo" />
+          </Box>
+
+          {/* ------------------------ */}
+
+          <Box
+            component="div"
+            sx={{
+              padding: { xs: 1, lg: "30px 20px", xl: "16px 10px" },
+              display: "flex",
+              flexDirection: "column",
+              alignItems: { xs: "center", xl: "start" },
+              px: { xs: "0px", lg: "117px", xl: "" },
+            }}
+          >
+            <Typography
+              component="h1"
+              sx={{
+                fontSize: "18px",
+                fontWeight: "bold",
+                paddingBottom: "10px",
+                color: "#202020",
+              }}
+            >
+              Send us massage via Email
+            </Typography>
+            <Typography
+              component="p"
+              sx={{
+                fontSize: "16px",
+                color: "#636363",
+                width: { xs: "255px", sm: "283px" },
+                textAlign: { xs: "center", xl: "start" },
+              }}
+            >
+              It is a long established fact that a reader will be by content.
+            </Typography>
+            <Box
+              component="div"
+              sx={{
+                display: "flex",
+                alignItems: { sm: "center" },
+                justifyContent: "space-between",
+                flexDirection: { xs: "column", sm: "row" },
+              }}
+            >
+              <Box
+                component="h1"
                 sx={{
-                  color: "#BEBEBF",
-                  padding: "10px",
-                  paddingRight: { xs: "20px", sm: "80px" },
+                  border: "2px solid #003566",
+                  borderRadius: "4px",
+                  width: "auto",
+                  marginRight: { sm: "15px" },
                 }}
               >
-                enter Your Email
-              </Typography>
+                <Typography
+                  component="p"
+                  sx={{
+                    color: "#BEBEBF",
+                    padding: "10px",
+                    paddingRight: { xs: "20px", sm: "80px" },
+                  }}
+                >
+                  enter Your Email
+                </Typography>
+              </Box>
+              <Subscribe>SUBSCRIBE</Subscribe>
             </Box>
-            <Subscribe>SUBSCRIBE</Subscribe>
           </Box>
+
+          {/* ------------- */}
         </Box>
       </Box>
       <Box
